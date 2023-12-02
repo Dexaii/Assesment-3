ffa457c25b62577920ebd0112cd5d06fad3c57da
Yurii Vladyko <yuriivladyko@gmail.com>
2023-12-01 19:37:28 -0800

    JS/firebase

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBb4SeEAvNb-3YIHlhWoqPVSFrqbD258Kc",
+    authDomain: "assesment-3-f26d0.firebaseapp.com",
+    projectId: "assesment-3-f26d0",
+    storageBucket: "assesment-3-f26d0.appspot.com",
+    messagingSenderId: "566813903694",
+    appId: "1:566813903694:web:352ca03ba1db20eecfac1f",
+    measurementId: "G-ZC04RG7LRF"
+  };
+  firebase.initializeApp(firebaseConfig);
